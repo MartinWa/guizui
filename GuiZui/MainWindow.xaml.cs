65df22522600b39833c13a76e87a68eaea91837b
Martin Wåger <martinw@gmail.com>
2013-04-18 20:56:04 +0200

    hej

--- GuiZui/MainWindow.xaml.cs
+++ GuiZui/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace GuiZui
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hej");
+        }
     }
 }
